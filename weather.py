69523692d309ccf4c1633829c44447010b232f9d
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-04-26 11:49:46 +0400

    code with input location

--- weather.py
+++ weather.py
@@ -1,7 +1,11 @@
 import requests
 
-url = 'https://wttr.in/san%20francisco?nTqu&lang=en'
-response = requests.get(url)
+# url = 'https://wttr.in/san%20francisco?nTqu&lang=en'
 
+url_template = 'https://wttr.in/{}'
+
+article_id = input('Input location: ')
+url = url_template.format(article_id)
+response = requests.get(url)
 
 print(response.text)
