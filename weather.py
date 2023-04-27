c27b82740dd6f79cb5aca5546b3bfaab5bd52d1b
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-04-27 14:17:39 +0400

    add_properties_dict

--- weather.py
+++ weather.py
@@ -1,16 +1,15 @@
 import requests
 
 
-def get_weather(loc):
-    url_template = 'https://wttr.in/{}?n?q?M?m?T'
-    lang = {'lang': 'ru'}
+def check_weather(loc):
+    url_template = 'https://wttr.in/{}'
+    display_properties = {'?n': '', '?q': '', '?M': '', '?m': '', '?T': '', 'lang': 'ru'}
     url = url_template.format(loc)
-    response = requests.get(url, params=lang)
+    response = requests.get(url, params=display_properties)
     return response.text
 
 
 location_list = ['London', 'cvo', 'Череповец']
 
 for location in location_list:
-    print(get_weather(location))
-    
+    print(check_weather(location))
